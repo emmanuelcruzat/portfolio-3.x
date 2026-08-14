8f257a729edaaaab88e4424e14c02cc24e114ba7
Emmanuel Cruzat <etc@njit.edu>
2026-08-14 10:11:59 -0400

    moved javascript to separate files

---
+++ typed-init.js
@@ -0,0 +1,19 @@
+new Typed("#typed-greeting", {
+  strings: [
+    "Hi, I'm Emmanuel!",
+    "Hallo, ich bin Emmanuel!",
+    "Hola, soy Emmanuel!",
+    "Bonjour, je suis Emmanuel!",
+    "Ciao, sono Emmanuel!",
+    "Привет, меня зовут Эммануэль!",
+    "こんにちは、エマニュエルです！",
+  ],
+  typeSpeed: 55,
+  backSpeed: 35,
+  backDelay: 1200,
+  startDelay: 300,
+  loop: true,
+  smartBackspace: true,
+  showCursor: true,
+  cursorChar: "|",
+});
